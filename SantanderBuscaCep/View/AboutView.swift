3a5b33f92d4fad7590d80feba96548a64d0d5e62
Josué Amorim <josuedesouzaamorim@gmail.com>
2023-10-06 09:33:07 -0300

    Created AboutView

--- SantanderBuscaCep/View/AboutView.swift
+++ SantanderBuscaCep/View/AboutView.swift
@@ -9,9 +9,26 @@ import SwiftUI
 
 struct AboutView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 50) {
+            HStack {
+                Image("Logo_Bootcamp")
+            }
+            VStack {
+                Image("Logo_Dio")
+                    .resizable()
+                    .frame(width: 100, height: 40)
+                
+                Text(verbatim: "www.dio.me/users/josuedesouzaamorim")
+                    .foregroundColor(.white)
+                   
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity) // Preenche toda a tela
+        .background(Color.santander)
+        .ignoresSafeArea() // Ignora a área segura (notch, barra de status, etc.)
     }
 }
+
 
 #Preview {
     AboutView()
