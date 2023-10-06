d14da158e16041591b90d4c4fa11f67ea476d9de
Josué Amorim <josuedesouzaamorim@gmail.com>
2023-10-06 13:56:16 -0300

    Enabling IQKeyboardManager

--- SantanderBuscaCep/SantanderBuscaCepApp.swift
+++ SantanderBuscaCep/SantanderBuscaCepApp.swift
@@ -6,12 +6,24 @@
 //
 
 import SwiftUI
+import IQKeyboardManagerSwift
 
 @main
 struct SantanderBuscaCepApp: App {
     var body: some Scene {
+        
         WindowGroup {
             ContentView()
         }
     }
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Outras configurações do aplicativo
+
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
+        
+        return true
+    }
+
 }
